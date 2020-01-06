d5ce85c8406693d2f8d73d7561675687edd13fd0
kwabe007 <kwabena.ap@gmail.com>
2020-01-06 23:21:42 +0100

    Change println to print

--- src/main/java/TrainPrinter.java
+++ src/main/java/TrainPrinter.java
@@ -1,15 +1,17 @@
 public class TrainPrinter {
 
     public static void printTrain(Train train) {
+
         for (int i = 0; i < train.size(); i++) {
             if (train.isSeatAvailable(i)) {
-                System.out.println(" " + (i + 1) + " ");
+                System.out.print(" " + (i + 1) + " ");
             } else if (train.isSeatBooked(i)) {
-                System.out.println("*" + (i + 1) + "*");
+                System.out.print("*" + (i + 1) + "*");
             } else {
-                System.out.println("?" + (i + 1) + "?");
+                System.out.print("?" + (i + 1) + "?");
             }
         }
+        System.out.println("");
     }
 
 }
